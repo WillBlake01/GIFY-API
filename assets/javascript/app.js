80c7d218070809916d6942596723effe7ef918e3
William Blake <willblakebooking@gmail.com>
2017-12-06 23:00:58 -0600

    more functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,11 +15,10 @@ $("#input").on("submit", function() {
   $("#buttons").append("<button class='band-name'>" + topics.splice(-1) + "</button>");
 
   $(topics).add(band);
-  
+
   $("button").on("click", function() {
 
     $("#gifs-appear-here").empty();
-    
       var bandImg = $(this).text();
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         bandImg + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -51,4 +50,38 @@ $("#input").on("submit", function() {
       });
 
     });
+
+$("button").on("click", function() {
+
+    $("#gifs-appear-here").empty();
+      var bandImg = $(this).text();
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        bandImg + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+          var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var bandImage = $("<img>");
+            bandImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(bandImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
+          }
+        });
+
+      });
+
 });
